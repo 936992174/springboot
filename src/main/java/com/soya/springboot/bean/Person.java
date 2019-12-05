396e2025e074442dd5f590542f2b6786316d316f
7287 <7287@jics.cn>
2019-12-05 17:46:10 +0800

    日志配置测试

--- src/main/java/com/soya/springboot/bean/Person.java
+++ src/main/java/com/soya/springboot/bean/Person.java
@@ -1,5 +1,6 @@
 package com.soya.springboot.bean;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +12,7 @@ import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "person")
 @Validated
+@Data
 public class Person {
 
     private String name;
@@ -20,62 +22,6 @@ public class Person {
     private Map<String,Object> maps;
     private List<Object> objects;
     private Dog dog;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public boolean isBoss() {
-        return isBoss;
-    }
-
-    public void setBoss(boolean boss) {
-        isBoss = boss;
-    }
-
-    public Map<String, Object> getMaps() {
-        return maps;
-    }
-
-    public void setMaps(Map<String, Object> maps) {
-        this.maps = maps;
-    }
-
-    public List<Object> getObjects() {
-        return objects;
-    }
-
-    public void setObjects(List<Object> objects) {
-        this.objects = objects;
-    }
-
-    public Dog getDog() {
-        return dog;
-    }
-
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
-
-    public Date getBirth() {
-        return birth;
-    }
-
-    public void setBirth(Date birth) {
-        this.birth = birth;
-    }
 
     @Override
     public String toString() {
--- src/test/java/com/soya/springboot/MyFirstApplicationTest.java
+++ src/test/java/com/soya/springboot/MyFirstApplicationTest.java
@@ -3,6 +3,8 @@ package com.soya.springboot;
 import com.soya.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,10 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyFirstApplicationTest {
+
+    private Logger logger = LoggerFactory.getLogger(MyFirstApplicationTest.class);
     @Autowired
     Person person;
     @Test
     public void loadContext(){
-        System.out.println(person);
+        logger.trace("这是trace:{}","hello");
+        logger.info(person.toString());
     }
 }
